92884a13977f926db1da008e0af1e75983ee86e0
Abhilaksh Chauhan <abhilaksh@StarLight.local>
2026-08-02 00:38:25 +0530

    feat: add unified action and activity lifecycle

--- types/runtime-globals.d.ts
+++ types/runtime-globals.d.ts
@@ -3,9 +3,11 @@ export {};
 declare global {
   var chrome: any;
   var ApplyOS: any;
+  var ScoutDialog: any;
   function importScripts(...urls: string[]): void;
   interface GlobalThis {
     ApplyOS: any;
     chrome: any;
+    ScoutDialog: any;
   }
 }
